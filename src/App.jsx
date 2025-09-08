1db07e839fb71f2b8cc14367c4f9341a1514b58f
islom upravleniya <islomanvarov05@gmail.com>
2025-09-08 09:32:25 +0500

    mains

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { AppleSidebar } from "./Main/sidebarMain";
+import { Outlet, useNavigate } from "react-router-dom";
+import LoginForm from "./pages/Auth/login";
+import { useGetDepQuery } from "./services/api";
+import Loading from "./components/loading/loading";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const navigate = useNavigate();
+  const [token, setToken] = useState(localStorage.getItem("tokens"));
+  const { data, isLoading, isError } = useGetDepQuery();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newToken = localStorage.getItem("tokens");
+      setToken(newToken);
+      // if (error)
+      if (!newToken) {
+        navigate("/login");
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [navigate]);
+
+  if (isLoading)
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <Loading />
+      </div>
+    );
+
+  // if (isError) return navigate("/login");
+
+  if (!token) {
+    return <LoginForm />;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen overflow-hidden">
+      <AppleSidebar />
+      <div className="w-full">
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// __dirname muqobili
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // hamma IP-lardan kirishga ruxsat beradi
+    port: 5174, // kerak bo‘lsa portni o‘zgartiring
+  },
+});
