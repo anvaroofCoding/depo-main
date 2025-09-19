b798a4142516d4719ff94d3f4efd8d092b49d9ae
islom upravleniya <islomanvarov05@gmail.com>
2025-09-19 10:55:18 +0500

    mains

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.10.41:8090/api",
+    baseUrl: "https://depotex.onrender.com/api",
     credentials: "include", // cookie ishlatadi
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("tokens");
